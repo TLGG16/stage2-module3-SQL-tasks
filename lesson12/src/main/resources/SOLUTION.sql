11396aa49360b77d0ed6e4dbefab20e5f3178574
TLGG16 <TLGG16@yandex.ru>
2023-08-12 20:18:16 +0300

    qweqwe

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,8 +1,3 @@
---DELETE FROM mark WHERE student_id IN (SELECT student_id FROM mark WHERE subject_id IN (SELECT subject_id from subject WHERE grade>=4));
---DELETE FROM student WHERE id IN (SELECT student_id FROM mark WHERE subject_id IN (SELECT subject_id from subject WHERE grade>=4));
---DELETE FROM mark WHERE student_id IN (SELECT id from student where groupnumber >=4);
---DELETE FROM payment WHERE student_id IN (SELECT id from student where groupnumber >=4);
---DELETE FROM student WHERE groupnumber >=4;
 DELETE mark, payment, student FROM mark
 JOIN payment ON mark.student_id = payment.student_id
 JOIN student ON mark.student_id = student.id
@@ -10,10 +5,6 @@ WHERE student.groupnumber >= 4;
 
 
 DELETE FROM student where id in (select student_id from mark where mark<4);
---DELETE FROM mark WHERE mark < 4;
-
---DELETE FROM payment WHERE type_id = (SELECT id FROM paymenttype WHERE name = 'Daily');
---DELETE FROM paymenttype WHERE name = 'Daily';
 DELETE payment, paymenttype FROM payment
 JOIN paymenttype ON payment.type_id = paymenttype.id
 WHERE paymenttype.name = 'Daily'
--- lesson3/src/main/resources/SOLUTION.sql
+++ lesson3/src/main/resources/SOLUTION.sql
@@ -1,16 +1,20 @@
-ALTER TABLE student ALTER COLUMN birthday DATE NOT NULL;
+ALTER TABLE student drop COLUMN birthday;
+ALTER TABLE student add COLUMN birthday DATE NOT NULL;
 
-ALTER TABLE mark ALTER COLUMN mark INT CHECK (mark >= 1 AND mark <= 10);
-ALTER TABLE mark ALTER COLUMN student_id BIGINT NOT NULL;
-ALTER TABLE mark ALTER COLUMN subject_id BIGINT NOT NULL;
+ALTER TABLE mark add CONSTRAINT CHK_const CHECK (mark >= 1 AND mark <= 10);
+ALTER TABLE mark drop constraint student_id;
+ALTER TABLE mark drop constraint subject_id;
+ALTER TABLE payment ADD CONSTRAINT student_id foreign key (student_id) references student(id) NOT NULL;
+ALTER TABLE payment ADD CONSTRAINT subject_id foreign key (subject_id) references subject(id) NOT NULL;
 
-ALTER TABLE subject ALTER COLUMN grade INT CHECK (grade >= 1 AND grade <= 5);
+ALTER TABLE subject add CONSTRAINT CHK_const2 CHECK (grade >= 1 AND grade <= 5);
+
 
 ALTER TABLE paymenttype ADD UNIQUE (name);
 
-ALTER TABLE payment ALTER COLUMN type_id BIGINT NOT NULL;
-ALTER TABLE payment ALTER COLUMN amount decimal NOT NULL;
-ALTER TABLE payment ALTER COLUMN payment_date DateTime NOT NULL;
+ALTER TABLE payment modify COLUMN type_id BIGINT NOT NULL;
+ALTER TABLE payment modify COLUMN amount decimal NOT NULL;
+ALTER TABLE payment modify COLUMN payment_date DateTime NOT NULL;
 
 ALTER TABLE payment ADD CONSTRAINT fk_payment_type FOREIGN KEY (type_id) REFERENCES paymenttype(id);
 
